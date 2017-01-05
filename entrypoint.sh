77eb42fc22053e0945c0479b979de5c3e2bc6a0c
Marc Lennox <marc.lennox@gmail.com>
2017-01-04 22:11:48 -0500

    Added directory creation for firmware directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,12 @@ set -e
 # Copy the configuration into place
 [ -z "${HOMIE_CONFIG_PATH}" ] || export INIFILE="${HOMIE_CONFIG_PATH}"
 
+# Create firmware directory
+[ -z "${INIFILE}" ] || (
+dirname $(cat ${INIFILE} | grep OTA_FIRMWARE_ROOT | sed 's/OTA_FIRMWARE_ROOT.*=\s*//')
+  mkdir -p "$(cat ${INIFILE} | grep OTA_FIRMWARE_ROOT | sed 's/OTA_FIRMWARE_ROOT.*=\s*//')"
+)
+
 # Prefix commands if none specified
 if [ -z "$@" ]; then
   set -- python /opt/homie-ota/homie-ota.py "$@"
